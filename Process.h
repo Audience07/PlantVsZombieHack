086b2331aa19897da45f782ba4987c30568a797a
Xue_001 <2894410545@qq.com>
2025-03-04 22:19:18 +0800

    .

--- Process.h
+++ Process.h
@@ -2,16 +2,15 @@
 #include "stdafx.h"
 //打开进程,失败返回NULL
 HANDLE xOpenProcessByName(IN LPCSTR lpClassName, IN LPCSTR lpWindowName) {
-	HANDLE Error = NULL;
 	DWORD ProcessID;
 	HWND hWnd = FindWindow(lpClassName, lpWindowName);
 	if (!hWnd) {
-		return Error;
+		return NULL;
 	}
 	GetWindowThreadProcessId(hWnd, &ProcessID);
 	HANDLE Process = OpenProcess(PROCESS_ALL_ACCESS, FALSE, ProcessID);
 	if (!Process) {
-		return Error;
+		return NULL;
 	}
 	return Process;
 }
