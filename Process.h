e5596333c0b0c55e25d4c007a9c186bfe33c9c20
Xue_001 <2894410545@qq.com>
2025-03-06 19:55:29 +0800

    Process

--- Process.h
+++ Process.h
@@ -1,7 +1,9 @@
 #pragma once
 #include "stdafx.h"
+#include <tlhelp32.h>
+
 //打开进程,失败返回NULL
-HANDLE xOpenProcessByName(IN LPCSTR lpClassName, IN LPCSTR lpWindowName) {
+HANDLE xOpenProcessByWndName(IN LPCSTR lpClassName, IN LPCSTR lpWindowName) {
 	DWORD PID;
 	HWND hWnd = FindWindow(lpClassName, lpWindowName);
 	if (!hWnd) {
@@ -13,4 +15,35 @@ HANDLE xOpenProcessByName(IN LPCSTR lpClassName, IN LPCSTR lpWindowName) {
 		return NULL;
 	}
 	return hProcess;
+}
+
+HANDLE xOpenProcessByPcName(IN LPCSTR PcName) {
+	DWORD dwProcessID = NULL;
+
+
+	HANDLE hSnapShot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	if (!hSnapShot) {
+		return NULL;
+	}
+	PROCESSENTRY32 pe32;
+	pe32.dwSize = sizeof(PROCESSENTRY32);
+	if (!Process32First(hSnapShot, &pe32)) {
+		return NULL;
+	}
+	while (Process32Next(hSnapShot, &pe32)) {
+		if (!(_tcsicmp(PcName, pe32.szExeFile))) {
+			dwProcessID = pe32.th32ProcessID;
+			break;
+		}
+	}
+	CloseHandle(hSnapShot);
+	//没有匹配到相同的进程名称
+	if (!dwProcessID) {
+		return NULL;
+	}
+	HANDLE hProcessID = OpenProcess(PROCESS_ALL_ACCESS, FALSE, dwProcessID);
+	if (!hProcessID) {
+		return NULL;
+	}
+	return hProcessID;
 }
